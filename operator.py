94e00fc8a94a543562f81a47699af1d0bd7fef52
stacksmith17 <pandeyarchit1706@gmail.com>
2026-01-22 10:52:48 +0530

    intial commit

---
+++ operator.py
@@ -0,0 +1,27 @@
+a = 12 
+b= 10 
+print (a+b)
+print (a-b)
+print (a*b)
+print (a/b)
+print (a%b)
+print (a//b)
+print (a**b)
+
+
+
+# assignment operator are used to assign the values to the variable 
+c = 9
+# this is simple assignment operator 
+ # compound assignment operator 
+
+c+=c #THIS MEANS THAT C = C+C
+
+print (c)
+
+
+
+# comparison operator for comaparing the two values or variable comparison operator will always return the boolean values 
+# logical operator and , or , not  
+print (123>88 and 22<66)
+print (not 9==9)
---
+++ strings.py
@@ -0,0 +1,15 @@
+
+
+
+ 
+b = "hello brother"
+print (b[0],b[-9])
+a= "sher coder "
+print(a[0:4:1]) #slicing 
+
+print (a[5::1])
+c = 12 
+c= str(c) # this is type conversion 
+
+print (type(c))
+print (bool(c))
