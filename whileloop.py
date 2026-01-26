e3c475b2eb25c25b8efef34bf57dee7cd0d8374c
stacksmith17 <pandeyarchit1706@gmail.com>
2026-01-26 16:28:58 +0530

    new

---
+++ whileloop.py
@@ -0,0 +1,6 @@
+# we have learnt the for loop works on number without the condition while the loop works on the condition 
+#  syntax for loop is while condition : then the code than is to be executed 
+a = 1
+while a <=30 :
+    print (a) 
+    a = a+1
