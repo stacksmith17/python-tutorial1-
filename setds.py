4720a879a26e48f5f86abcabeda1ae5e003139ca
stacksmith17 <pandeyarchit1706@gmail.com>
2026-01-28 19:55:37 +0530

    updated

---
+++ setds.py
@@ -0,0 +1,23 @@
+# sets is an unordered collection of unique elements 
+# set is defined with {}
+
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+# operation are all those we use in the mathematical sets 
+# union of sets 
+A|B
+print (A|B) #or we can use the A.union(B)
+
+#intersection of sets 
+print (A&B) # or we can use the A.intersection(B)
+
+#difference in sets 
+print (A-B)
+
+#Subset
+print (A<=B)
+# we can also use a.issuperset(b)
+
+# disjoint 
+A.isdisjoint(B)
