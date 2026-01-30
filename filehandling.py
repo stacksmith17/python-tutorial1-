33d427e00dbef2adea7d22e0da0b7779facc664b
stacksmith17 <pandeyarchit1706@gmail.com>
2026-01-30 17:11:02 +0530

    file handling

---
+++ filehandling.py
@@ -0,0 +1,10 @@
+"""this file handling helps in handling the file in the python 
+there are many ways to do operations in the files  
+r - reading the files
+w - writes (overwrites files )
+a- append 
+x - create the new files 
+r+ - read and write 
+w+ - write and read 
+b - binary mode 
+"""
